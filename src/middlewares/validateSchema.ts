20a285ce60b3e7846972ba6ff7ea2c5611c63d59
PedroStaRosa <pedro.santarosa@outlook.com>
2026-02-23 18:08:07 -0300

    feat:  bodyless request error handling

--- src/middlewares/validateSchema.ts
+++ src/middlewares/validateSchema.ts
@@ -3,27 +3,36 @@ import { ZodError, ZodType } from "zod";
 
 export const validateSchema =
   (schema: ZodType) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await schema.parseAsync({
-        body: req.body,
-        query: req.query,
-        params: req.params,
-      });
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        await schema.parseAsync({
+          body: req.body,
+          query: req.query,
+          params: req.params,
+        });
 
-      return next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        return res.status(400).json({
-          error: "Erro validação",
-          details: error.issues.map((issue) => ({
-            message: issue.message,
-          })),
+        return next();
+      } catch (error) {
+
+        if (error instanceof ZodError) {
+          const isMissingBody = error.issues.some(
+            (issue) => issue.message === "Invalid input: expected object, received undefined"
+            // ou issue.code === "invalid_type" e issue.received === "undefined"
+          );
+          if (isMissingBody) {
+            return res.status(400).json({
+              error: "Erro validação",
+              details: [{ message: "Corpo da requisição é obrigatório. Envie um JSON com o campo(s) obrigatório(s). Verifique na documentação para mais informações." }],
+            });
+          }
+          return res.status(400).json({
+            error: "Erro validação",
+            details: error.issues.map((issue) => ({ message: issue.message })),
+          });
+        }
+
+        return res.status(500).json({
+          error: "Erro interno do servidor",
         });
       }
-
-      return res.status(500).json({
-        error: "Erro interno do servidor",
-      });
-    }
-  };
+    };
